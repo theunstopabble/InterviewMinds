e1b73b2bccb38388022e5f08d083a1c4f9752fa3
theunstopabble <gautamkumar43421@gmail.com>
2026-01-18 01:15:49 +0530

    FIX: Stabilized dependencies & Added Split Screen Editor

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    "@tailwindcss/postcss": {}, // <--- Ab hum naya wala plugin use kar rahe hain
+    tailwindcss: {}, // <--- Sirf 'tailwindcss' likhna hai, '@tailwindcss/postcss' nahi!
     autoprefixer: {},
   },
 };
