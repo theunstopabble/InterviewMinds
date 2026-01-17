32b5d5c7e3ece3593c58f0368dd2b9e4f0123127
theunstopabble <gautamkumar43421@gmail.com>
2026-01-18 05:13:49 +0530

    FIX: Update Navbar logo to use PWA icon and remove Sparkles

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1ing8p67t5"
+    "revision": "0.evjb3k3cn6c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
